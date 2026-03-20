4dfec0a9b4cd4cd2dffa9c4f354d936927f470f9
DRSJir <jairdelreal@gmail.com>
2026-03-20 11:01:31 -0600

    Commit inicial

---
+++ src/main/java/cosmicos/App.java
@@ -0,0 +1,16 @@
+package cosmicos;
+
+import io.javalin.Javalin;
+import io.javalin.http.staticfiles.Location;
+
+public class App {
+
+	public static void main(String[] args) {
+		var app = Javalin.create(config -> {
+            // Esto le dice a Javalin que busque archivos en src/main/resources/public
+            config.staticFiles.add("/public/inicio.html", Location.CLASSPATH);
+        }).start("0.0.0.0",8080);
+
+	}
+
+}
